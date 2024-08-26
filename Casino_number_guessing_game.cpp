f8f94553bfdf717252beff1e5dca391c8f5fd03c
Michaltntk <michaltntk@gmail.com>
2024-08-26 16:32:31 +0200

    Doing some polishing: deleted file.txt and file2 because are unnecessary Made function called answering under main function to made more redable and understandable fixed loop and allowed to make mistakes while answering

--- Casino_number_guessing_game.cpp
+++ Casino_number_guessing_game.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
+#include <cstdio>
 
 using namespace std;
+
+string answering();
 
 int main()
 {
@@ -12,28 +15,47 @@ int main()
     cout << "Hello welcome to our casino. Come and play number guessing game by Michaltntk\n";
     do // Loop created so player can play again and quit
     {
-    cout << "Write your number: ";
-    cin >> guess_number; // inserting player number
-    //Checking if input is number or not
-    if(!cin) // or if(cin.fail())
-    {
-    // user didn't input a number
-    cin.clear(); // reset failbit
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
-    // next, request user reinput
-    }
-    srand(time(NULL)); // Initialiazing randomness 
-    computer_number = rand()%10+1; // assaining random number from 1 to 10 
-    cout << "Number that has been choosen by computer: " << computer_number << endl;
-    if (guess_number==computer_number)
-    {
-        cout << "Congratulations. You won\n";
-        cout << "Do you wanna play again?\n";
-        cin >> player_answer;
-        while(player_answer != "Yes" || player_answer != "Y" || player_answer != "No" || player_answer != "N")
+        cout << "Write your number: ";
+        cin >> guess_number; // inserting player number
+        //Checking if input is number or not
+        if(!cin) // or if(cin.fail())
         {
+            do
+            {
+            // user didn't input a number
+            cin.clear(); // reset failbit
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
+            // next, request user reinput
+            cout << "You must write a number: ";
+            cin >> guess_number; // inserting player number
+            }
+            while(!cin);
+        }
+        srand(time(NULL)); // Initialiazing randomness 
+        computer_number = rand()%10+1; // assaining random number from 1 to 10 
+        cout << "Number that has been choosen by computer: " << computer_number << endl;
+        if (guess_number==computer_number)
+        {
+            cout << "Congratulations. You won\n";
+            player_answer = answering();
+        }
+        else
+        {
+            cout << "Oh dang it\n";
+            player_answer = answering();
+        } 
+    }
+    while (player_answer=="Yes" || player_answer=="Y");
+    getchar();
+}
 
-        
+string answering()
+{
+    string player_answer;
+    cout << "Do you wanna play again?\n";
+        cin >> player_answer;
+        do
+        {
             if (player_answer == "Yes" || player_answer == "Y" || player_answer == "No" || player_answer == "N")
                 {
                     if (player_answer == "Yes" || player_answer == "Y")
@@ -44,45 +66,15 @@ int main()
                     else
                     {
                     cout << "U could have hit it big";
+                    break;
                     }
                 }
             else
             {
-                cout << "Please write answer in type of Yes or Y for confirmation, and No or N for decline";
+                cout << "Please write answer in type of Yes or Y for confirmation, and No or N for decline: ";
+                cin >> player_answer;
             }
         }
-    }
-    else
-    {
-        cout << "Oh dang it\n";
-        cout << "Do you wanna play again?\n";
-        cin >> player_answer;
-        {
-        if (player_answer == "Yes" || player_answer == "Y" || player_answer == "No" || player_answer == "N")
-        {
-        
-            if (player_answer == "Yes" || player_answer == "Y")
-            {
-                cout << "Let's go gambling\n";
-            }
-            else if(player_answer == "No" || "N")
-            {
-                cout << "U could have hit it big\n";
-            }
-
-        else
-            {
-            do
-                {
-                    cout << "Please write answer in type of Yes or Y for confirmation, and No or N for decline";
-                    cin >> player_answer;
-                }
-                while(player_answer != "Yes" || player_answer != "Y" || player_answer != "No" || player_answer != "N");
-            
-            }
-        }
-    }
-    
-    } 
-    }while (player_answer=="Yes" || player_answer=="Y");
+        while(player_answer != "Yes" || player_answer != "Y" || player_answer != "No" || player_answer != "N");
+        return player_answer;
 }
