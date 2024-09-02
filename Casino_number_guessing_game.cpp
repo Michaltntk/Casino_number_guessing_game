ace05185905ddda48671e64cb159e2c2ae3bd7dd
Michaltntk <michaltntk@gmail.com>
2024-09-02 19:05:55 +0200

    Some more polishing also fixing and adding stuff

--- Casino_number_guessing_game.cpp
+++ Casino_number_guessing_game.cpp
@@ -9,12 +9,44 @@ string answering();
 
 int main()
 {
+    int money = 200; // Money that player has
     int guess_number; // number guessed by player
     int computer_number; // number guessed by computer
+    int choice;
     string player_answer; // Player answer to if keep playing game still or quiting
+    srand(time(NULL)); // Initialiazing randomness
     cout << "Hello welcome to our casino. Come and play number guessing game by Michaltntk\n";
     do // Loop created so player can play again and quit
     {
+        cout << "You have: " << money << "$\n";
+        cout << "Please choose dificulty that you want: \n";
+        cout << "1. Easy play for 1$ Win 10$ Numbers are between 1-10\n";
+        cout << "2. Medium play for 10$ Win 100$ Numbers are between 1-20\n";
+        cout << "3. Hard play for 100$ Win 1000$ Numbers are between 1-50\n";
+        do
+        {
+            cin >> choice;
+            switch (choice)
+            {
+                case 1:
+                    computer_number = rand()%10+1; // assaining random number from 1 to 10
+                    money -= 1;
+                    break;
+                case 2:
+                    computer_number = rand()%20+1; // assaining random number from 1 to 20 
+                    money -= 10;
+                    break;
+                case 3:
+                    computer_number = rand()%50+1; // assaining random number from 1 to 50 
+                    money -= 100;
+                    break;
+                default:
+                    cout << "Please pick a correct option: \n";
+                    break;
+            }
+            
+        }
+        while(choice != 1 && choice != 2 && choice != 3);
         cout << "Write your number: ";
         cin >> guess_number; // inserting player number
         //Checking if input is number or not
@@ -31,19 +63,39 @@ int main()
             }
             while(!cin);
         }
-        srand(time(NULL)); // Initialiazing randomness 
-        computer_number = rand()%10+1; // assaining random number from 1 to 10 
+        
         cout << "Number that has been choosen by computer: " << computer_number << endl;
         if (guess_number==computer_number)
         {
             cout << "Congratulations. You won\n";
+            switch (choice)
+            {
+            case 1:
+                money += 10;
+                break;
+            case 2:
+                money += 100;
+                break;
+            case 3:
+                money += 1000;
+                break;
+            default:
+                break;
+            }
             player_answer = answering();
         }
         else
         {
             cout << "Oh dang it\n";
+            if (money == 0)
+            {
+            cout << "You can't play anymore, you don't have any money left\n";
+            break;
+            }
             player_answer = answering();
-        } 
+        }
+        
+        
     }
     while (player_answer=="Yes" || player_answer=="Y");
     system("pause");
